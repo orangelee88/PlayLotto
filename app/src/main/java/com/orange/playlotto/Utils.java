c33be9a5680e9d4092a486d1cb3dc1e56720acbe
OrangeLee <a0487387521@gmail.com>
2021-01-29 23:22:59 +0800

    Model panel fix

---
+++ app/src/main/java/com/orange/playlotto/Utils.java
@@ -0,0 +1,17 @@
+package com.orange.playlotto;
+
+public class Utils {
+    // 兩次點選按鈕之間的點選間隔不能少於1000毫秒
+    private static final int MIN_CLICK_DELAY_TIME = 1000;
+    private static long lastClickTime;
+
+    public static boolean isFastClick() {
+        boolean flag = false;
+        long curClickTime = System.currentTimeMillis();
+        if ((curClickTime - lastClickTime) <= MIN_CLICK_DELAY_TIME) {
+            flag = true;
+        }
+        lastClickTime = curClickTime;
+        return flag;
+    }
+}
--- app/src/main/java/com/orange/playlotto/WelCome.java
+++ app/src/main/java/com/orange/playlotto/WelCome.java
@@ -3,8 +3,10 @@ package com.orange.playlotto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class WelCome extends AppCompatActivity {
 
@@ -12,7 +14,6 @@ public class WelCome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_wel_come );
-
 
     }
 
